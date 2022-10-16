270730acdda82732813673d703d9eab72759d6a4
Michal Badin <xbadin@fi.muni.cz>
2022-10-16 21:58:12 +0200

    Added Infrastructure.EFCore

---
+++ BazaarDAL/Infrastructure.EFCore/EFCoreQueryObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bazaar.DAL.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bazaar.DAL.Infrastructure.EFCore
+{
+    public class EFCoreQueryObject<TEntity> : QueryObject<TEntity> where TEntity : class, new()
+    {
+        private DbContext _dbContext;
+
+        public EFCoreQueryObject(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _query = _dbContext.Set<TEntity>().AsQueryable();
+        }
+
+        public override async Task<IEnumerable<TEntity>> ExecuteAsync()
+        {
+            return await _query.ToListAsync();
+        }
+    }
+}
