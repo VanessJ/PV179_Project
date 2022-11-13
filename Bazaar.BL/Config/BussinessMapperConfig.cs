a029bec544e025992011113ed5f9cb33ac0ddf37
VanessJ <vanessa.joriova@gmail.com>
2022-11-13 22:26:49 +0100

    config

---
+++ Bazaar.BL/Config/BussinessMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar.BL.Config
+{
+    public class BussinessMapperConfig
+    {
+        public static void ConfigureMapping(IMapperConfigurationExpression config)
+        {
+           
+        }
+    }
+}
