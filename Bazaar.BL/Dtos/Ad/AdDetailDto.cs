1560af168786868c6507820c8e32ef13bb62cdc3
VanessJ <vanessa.joriova@gmail.com>
2022-11-18 12:49:04 +0100

    additional dto/filters

---
+++ Bazaar.BL/Dtos/Ad/AdDetailDto.cs
@@ -0,0 +1,30 @@
+﻿using Bazaar.BL.Dtos.Image;
+using Bazaar.BL.Dtos.Tag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar.BL.Dtos.Ad
+{
+    public class AdDetailDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public bool IsPremium { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public bool IsOffer { get; set; }
+
+        public int Price { get; set; }
+        public UserDto Creator { get; set; } = null!;
+
+        public ICollection<ImageDto> Images { get; set; }
+
+        public ICollection<TagDto> Tags { get; set; }
+    }
+}
--- Bazaar.BL/Dtos/Ad/AdFilterDto.cs
+++ Bazaar.BL/Dtos/Ad/AdFilterDto.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bazaar.BL.Dtos.Base;
 using Bazaar.BL.Dtos.Tag;
+using Optional;
 
 namespace Bazaar.BL.Dtos.Ad
 {
@@ -12,6 +14,13 @@ namespace Bazaar.BL.Dtos.Ad
     {
         public string? ContainsTitleName { get; set; }
         public string? LikeTitleName { get; set; }
+
+        public int MinPrice { get; set; } = -1;
+        public int MaxPrice { get; set; } = -1;
+
+        public bool IsValid { get; set; }
+
+        public bool IsOffer { get; set; }
 
         public string? ContainsInDescription { get; set; }
         public Guid? UserId { get; set; }
