cbaf200ec1540a58fde4a2e43c1bb0a58fe27c28
VanessJ <vanessa.joriova@gmail.com>
2022-12-06 12:03:44 +0100

    ad

---
+++ Bazaar.App/Controllers/AdController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Bazaar.App.Models;
+using Bazaar.BL.Dtos.Ad;
+using Bazaar.BL.Dtos.Tag;
+using Bazaar.BL.Facade;
+using Bazaar.BL.Services.Tags;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazaar.App.Controllers
+{
+    public class AdController : Controller
+    {
+        private readonly IAdFacade _adFacade;
+        private readonly IMapper _mapper;
+
+        public AdController(IAdFacade adFacade, IMapper mapper)
+        {
+            _adFacade = adFacade;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dto = new AdFilterDto();
+            var model = new AdIndexViewModel()
+            {
+                
+                Ads = await _adFacade.FilterAds(dto)
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Bazaar.App/Models/Ad/AdIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Bazaar.BL.Dtos.Ad;
+
+namespace Bazaar.App.Models
+{
+    public class AdIndexViewModel
+    {
+        public IEnumerable<AdListDto>? Ads { get; set; }
+    }
+}
